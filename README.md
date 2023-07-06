03561594fae8f0e3cbbb31fcb7352ef0b24e9685
echumila <edgarcha91@gmail.com>
2023-07-06 17:41:36 -0600

    first commit

---
+++ README.md
@@ -0,0 +1,13 @@
+# instalación de sitio
+# React + Vite
+
+# Instalar react
+# Instalar dependencias (npm)
+#       npm install
+# instalación de requirements.txt
+#       pip install -r requirements.txt
+# levantar el servicio
+#       npm run dev
+
+
+- VITE_BACKEND_URL, this is neccessary in production, it should be the url of the backend server
---
+++ src/pages/TaskFormPage.jsx
@@ -0,0 +1,103 @@
+import { useEffect } from "react";
+import { useForm } from "react-hook-form";
+import { useNavigate, useParams } from "react-router-dom";
+import { createTask, deleteTask, getTask, updateTask } from "../api/tasks.api";
+import { toast } from "react-hot-toast";
+
+export function TaskFormPage() {
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+    setValue,
+  } = useForm();
+  const navigate = useNavigate();
+  const params = useParams();
+
+  const onSubmit = handleSubmit(async (data) => {
+    if (params.id) {
+      await updateTask(params.id, data);
+      toast.success("Employee updated", {
+        position: "bottom-right",
+        style: {
+          background: "#101010",
+          color: "#fff",
+        },
+      });
+    } else {
+      await createTask(data);
+      toast.success("New Employee Added", {
+        position: "bottom-right",
+        style: {
+          background: "#101010",
+          color: "#fff",
+        },
+      });
+    }
+
+    navigate("/tasks");
+  });
+
+  useEffect(() => {
+    async function loadTask() {
+      if (params.id) {
+        const { data } = await getTask(params.id);
+        setValue("title", data.title);
+        setValue("description", data.description);
+      }
+    }
+    loadTask();
+  }, []);
+
+  return (
+    <div className="max-w-xl mx-auto">
+      <form onSubmit={onSubmit} className="bg-zinc-800 p-10 rounded-lg mt-2">
+        <input
+          type="text"
+          placeholder="Nombre"
+          {...register("title", { required: true })}
+          className="bg-zinc-700 p-3 rounded-lg block w-full mb-3"
+          autoFocus
+        />
+
+        {errors.title && <span>This field is required</span>}
+
+        <textarea
+          placeholder="Salario"
+          {...register("description", { required: true })}
+          className="bg-zinc-700 p-3 rounded-lg block w-full"
+        />
+
+        {errors.description && <span>This field is required</span>}
+
+        <button className="bg-indigo-500 p-3 rounded-lg block w-full mt-3">
+          Guardar
+        </button>
+      </form>
+
+      {params.id && (
+        <div className="flex justify-end">
+          <button
+            className="bg-red-500 p-3 rounded-lg w-48 mt-3"
+            onClick={async () => {
+              const accepted = window.confirm("Are you shure?");
+              if (accepted) {
+                await deleteTask(params.id);
+                toast.success("Employee Deleted", {
+                  position: "bottom-right",
+                  style: {
+                    background: "#101010",
+                    color: "#fff",
+                  },
+                });
+                navigate("/tasks");
+              }
+            }}
+          >
+            Eliminar
+          </button>
+        </div>
+      )}
+    </div>
+  );
+}
